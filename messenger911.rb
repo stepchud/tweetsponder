c4aff5448cd2a76bfce1d0bc0316ac7f673b0204
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2017-12-09 21:09:03 -0800

    renamed env var

--- messenger911.rb
+++ messenger911.rb
@@ -7,10 +7,8 @@ end
 
 get '/sf911' do
   puts "params=#{params}"
-  puts "ENV=#{ENV.to_h}"
-  return params
-  puts ENV['FB_CONFIG_TOKEN']
-  if ENV['FB_CONFIG_TOKEN'] == params['hub.verify']
+  puts ENV['FB_VERIFY_TOKEN']
+  if ENV['FB_VERIFY_TOKEN'] == params['hub.verify']
     puts "verified!"
     params['hub.challenge']
   else
