7df300ddc1656d34fe80fef9da9598f4f99511a7
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2017-12-09 21:11:39 -0800

    correct param name

--- messenger911.rb
+++ messenger911.rb
@@ -8,9 +8,9 @@ end
 get '/sf911' do
   puts "params=#{params}"
   puts ENV['FB_VERIFY_TOKEN']
-  if ENV['FB_VERIFY_TOKEN'] == params['hub.verify']
+  if ENV['FB_VERIFY_TOKEN'] == params["hub.verify_token"]
     puts "verified!"
-    params['hub.challenge']
+    params["hub.challenge"]
   else
     403
   end
