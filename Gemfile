2dd58eb2cb02c3dcf6da1d0cf5b16373fc4f5fa4
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2017-12-10 09:17:05 -0800

    echo response

--- Gemfile
+++ Gemfile
@@ -6,4 +6,4 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra"
-gem "facebook-messenger"
+gem "httparty"
--- messenger911.rb
+++ messenger911.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
-require 'facebook/messenger'
+require 'json'
+require 'httparty'
+
+CHATS = {}
 
 get '/' do
   "Hello Homie"
@@ -17,6 +20,39 @@ get '/sf911' do
 end
 
 post '/sf911' do
-  puts "GOT WEBHOOK #{request.body.string}"
+  @message_body = JSON.parse(request.body)
+  puts @message_body
+  CHATS[sender_id] ||= {messages:[]}
+  CHATS[sender_id][:messages] << message_text
+  HTTParty.post(response_url, body: response(message_text))
   200
+end
+
+private
+
+def response_url
+  "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV['PAGE_ACCESS_TOKEN']}"
+end
+
+def messaging
+  @message_body["entry"]["messaging"].first
+end
+
+def message_text
+  messaging["message"]["text"]
+end
+
+def sender_id
+  messaging["sender"]["id"]
+end
+
+def fb_user
+  "https://wwww.facebook.com/#{sender_id}"
+end
+
+def response text
+  {
+    "recipient": {"id": sender_id},
+    "message": {"text": text}
+  }
 end
